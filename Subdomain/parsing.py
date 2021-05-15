17c6ada0b31a1e3ba6d16f41cd895c242b8c108b
Hmjii <guasls4663@dgu.ac.kr>
2021-05-15 23:49:13 +0900

    now can find student and mentor

--- Subdomain/parsing.py
+++ Subdomain/parsing.py
@@ -3,19 +3,19 @@ from bs4 import BeautifulSoup
 from collections import deque
 import traceback
 import requests
+import os
 
 
 def search_tag(path):
     visited.add(path)
-
     try:
         path_name = path.replace('/', '_')
         path_name = path_name.replace('?', '_')
-        with open(f"ahref{path_name}.txt", "w", encoding='UTF-8') as f:  # ,기준으로 text, url
+        with open(f"{folder} ahref{path_name}.txt", "w", encoding='UTF-8') as f:  # ,기준으로 text, url
             f.write("text, url 순\n\n")
             absolute_path = ROOT + path
             print("absolute_path :", absolute_path)
-
+            
             ###########################################
             res=session.get(absolute_path,timeout=4)        #기존에서 session.get으로 로그인 상태 유지한채 받는거로 바꿈
             soup = BeautifulSoup(res.content, "html.parser")
@@ -66,7 +66,7 @@ def search_tag(path):
                 f.write(url)
                 f.write("\n")
 
-        with open(f"form{path_name}.txt", "w", encoding='UTF-8') as f: # method, action순
+        with open(f"{folder}form{path_name}.txt", "w", encoding='UTF-8') as f: # method, action순
             f.write("method, action 순\n\n")
             for link in soup.find_all('form'):
                 method = link.get('method')
@@ -84,7 +84,7 @@ def search_tag(path):
                 f.write(method)
                 f.write("\n")
 
-        with open(f"input{path_name}.txt", "w", encoding='UTF-8') as f: # text,type,placeholder 순
+        with open(f"{folder}input{path_name}.txt", "w", encoding='UTF-8') as f: # text,type,placeholder 순
             f.write("text, type, placeholder 순\n\n")
             for link in soup.find_all('input'):
                 text, type, placeholder = link.text.strip(), link.get('type'), link.get('placeholder')
@@ -103,46 +103,43 @@ def search_tag(path):
                 f.write(placeholder)
 
                 f.write("\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     except:
         print(traceback.format_exc().splitlines()[-1])
 
 
 if __name__ == "__main__":
-    ROOT = "http://ssms.dongguk.edu"
-    visited = set()
-    queue = deque([""])
 
-########################################################
-    login_url='http://ssms.dongguk.edu/mbrmgt/DGU121'
-    craw_url='http://ssms.dongguk.edu/pjmng/DGU522'
+    curdir=os.getcwd()
+    logintypes=['/student', '/mentor']#, 'instructor' ,'assistant']
+    for folder in logintypes:
+        directory= curdir+folder
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    values = {
+    loginfo=[]
+    stdvalues = {
         'loginType': 'student',        #student, mentor,instructor,assistant
         'loginId': '2018113581',       #학생, 멘토, 교수, 조교 4가지 logintype
         'loginPwd': 'caps1234'
-        }
+    }
+    mtrvalues={
+        'loginType': 'mentor',        
+        'loginId': 'juno',      
+        'loginPwd': 'theori1234'
+    }
+    loginfo.append(stdvalues)
+    loginfo.append(mtrvalues)
 
+for login in loginfo:
+
+    ROOT = "http://ssms.dongguk.edu"
+    visited = set()
+    queue = deque([""])
+    login_url='http://ssms.dongguk.edu/mbrmgt/DGU121'
     session = requests.session()
-    res=session.post(login_url,data=values)
-    res.raise_for_status()  
-######################################################  login 부분 student이외 계정을 받으면 for loop로 다시 수정
-
+    res=session.post(login_url,data=login)
+    res.raise_for_status() 
+    folder=login.get('loginType')+'/'
     while queue:
         u = queue.popleft()
         if u not in visited:
