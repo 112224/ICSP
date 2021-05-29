7f4909996483cbd3201db9eff41604948f9a07d3
112224 <didgns25@naver.com>
2021-05-29 17:42:23 +0900

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Subdomain/parsing_yh.py
    Minor update

--- Subdomain/parsing_yh.py
+++ Subdomain/parsing_yh.py
@@ -7,11 +7,13 @@ import os
 import pandas as pd
 import json
 
+
 def refine_str(dirty):
     clean = dirty.replace('"', '')
     clean = clean.replace("'", '')
     clean = clean.replace(',', '')
     return clean
+
 
 # herf tag 를 만나면 => q에 추가
 # path => 현재 url
@@ -126,7 +128,7 @@ if __name__ == "__main__":
         if not os.path.exists(directory):
             os.makedirs(directory)'''
 
-    loginfo = []
+    loginfo = [{}]
     # 사용자 입력을 받거나, 파일을 읽어오는 방식으로 변경
     # ex) 옵션을 줘서 읽어올 파일이 있으면 읽어오고 아닐 경우 입력을 받는 방식
     with open('logininfos.json','r') as f:
@@ -170,6 +172,10 @@ if __name__ == "__main__":
                 print(traceback.format_exc().splitlines()[-1])
             once = True
         print('complete the task!!')
-        for ele in ret:
-            print(ele)
+        for path, method, action,req_params, sel_params in ret:
+            if action:
+                action = ROOT + action
+            # req_params 는 이름만 있다고 가정
+            print(path, method, action, req_params, sel_params)
+            path + "--data="+'=1&'.join(req_params)
         # df.to_csv(f'{ROOT}.csv', index=False)
